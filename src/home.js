beb78efbdc316c9d1321990606525ff36dfbc55c
kerimfc <kerimfahricakmak@hotmail.com>
2023-03-11 16:26:37 +0300

    UseEffect

--- src/home.js
+++ src/home.js
@@ -3,45 +3,18 @@ import Bloglist from "./BlogList";
 
 const Home = () => {
   
-  const[blogs, setBlogs] = useState([
-    {
-      ad:"Yeni Başlayanlar İçin C#",
-      aciklama:"Lorem ipsum dolar sit amet",
-      yazar:"luffy",
-      id:1
-    },
-    {
-      ad:"React öğreniyorum",
-      aciklama:"Lorem ipsum dolar sit amet",
-      yazar:"zoro",
-      id:2
-    },
-    {
-      ad:"İleri seviye C#",
-      aciklama:"Lorem ipsum dolar sit amet",
-      yazar:"luffy",
-      id:3
-    },
-    {
-      ad:"MVC öğreniyorum",
-      aciklama:"Lorem ipsum dolar sit amet",
-      yazar:"sanji",
-      id:4
-    },
-    {
-      ad:"Flutter Giriş Seviye",
-      aciklama:"Lorem ipsum dolar sit amet",
-      yazar:"develop",
-      id:5
-    }
-  ])
+  const[blogs, setBlogs] = useState(null)
 
-  const [isim, setIsim] = useState('luffy');
 
   useEffect(()=>{
-    console.log("use effect calıştı");
-    console.log(blogs);
-  },[blogs, isim])
+   
+    fetch('http://localhost:8000/yazilar').then(res=>{
+      return res.json();
+    }).then(data=>{
+      //console.log(data);
+      setBlogs(data);
+    })
+  },[])
 
   const handleClick =(id)=>{
       const newBlogs = blogs.filter(blog=>blog.id != id);
@@ -53,11 +26,8 @@ const Home = () => {
 
     return (
        <div className="home">
-            <Bloglist bloglar={blogs} baslik="Bütün Yazılar" handleClick={handleClick}/>
+            {blogs && <Bloglist bloglar={blogs} baslik="Bütün Yazılar" handleClick={handleClick}/>}
             <br/>
-            <button onClick={()=>setIsim('zoro')}>değiştir</button>
-            <br/>
-            <Bloglist bloglar={blogs.filter((blog) =>  blog.yazar == 'luffy')} baslik="Seçkin Yazılar" />
        </div>
       );
 }
