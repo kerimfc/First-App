3408e982cc1bba3c9176da2bf44de0b15af0fafd
kerimfc <kerimfahricakmak@hotmail.com>
2023-03-11 18:49:07 +0300

    UseEffect

--- src/home.js
+++ src/home.js
@@ -1,44 +1,15 @@
-import { useState , useEffect} from "react";
 import Bloglist from "./BlogList";
+import useFetch from "./usefetch";
 
 const Home = () => {
   
-  const[blogs, setBlogs] = useState(null)
-  const[yukleniyor , setYukleniyor] = useState(true)
-  const[hata, setHata] = useState(null)
-
-
-  useEffect(()=>{
-   
-    fetch('http://localhost:8000/yazilar').then(res=>{
-      if(!res.ok) throw Error("Veriler çekilemedi")
-
-      return res.json();
-    }).then(data=>{
-      //console.log(data);
-      setBlogs(data);
-      setYukleniyor(false);
-    })
-    .catch(err=>{
-      setHata(err.message);
-      setYukleniyor(false);
-      //console.log(err);
-    })
-  },[])
-
-  const handleClick =(id)=>{
-      const newBlogs = blogs.filter(blog=>blog.id != id);
-
-      setBlogs(newBlogs);
-
-      console.log(id + " li Yazı silindi");
-  }
+  const{veri:blogs,yukleniyor,hata}=useFetch("http://localhost:8000/yazilar");
 
     return (
        <div className="home">
             {hata && <div className="error">{hata}</div>}
             {yukleniyor && <div className="loading">Yükleniyor..</div> }
-            {blogs && <Bloglist bloglar={blogs} baslik="Bütün Yazılar" handleClick={handleClick}/>}
+            {blogs && <Bloglist bloglar={blogs} baslik="Bütün Yazılar" />}
             <br/>
        </div>
       );
